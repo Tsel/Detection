a3b6d2ad5b4c20443429d136658a50dc54b47fb5
Tsel <thomas.selhorst@online.de>
2017-11-30 15:06:00 +0100

    video2images added

--- swiss.py
+++ swiss.py
@@ -2,3 +2,16 @@
 This file provides helper functions
 last modified 30-11-2017
 """
+
+import cv2
+
+def video2images(fn):
+    vidcap = cv2.VideoCapture(fn)
+    success, image = vidcap.read()
+    count = 0
+    success = True
+    while success:
+        success, image = vidcap.read()
+        print('Read new frame: ', success)
+        cv2.imwrite("frame%d.jpg" % count, image)
+        count += 1
